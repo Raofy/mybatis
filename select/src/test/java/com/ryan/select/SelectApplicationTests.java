013224f2f132e09ca8c0e25fad69a8f3684557b2
富沂 <1312170600@qq.com>
2020-09-03 10:45:55 +0800

    工程第一次初始化上传

---
+++ select/src/test/java/com/ryan/select/SelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.select;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
